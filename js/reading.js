239052cf2155aef3c6c7c34c036f5a0f829625b2
aiko030500-wq <aiko030500@gmail.com>
2025-10-29 01:08:34 +0500

    Update reading.js

--- js/reading.js
+++ js/reading.js
@@ -16,7 +16,7 @@
       <div class="nav-buttons">
         <button id="rBack">Back</button>
         <button id="rNext">Next</button>
-        <button onclick="show('menu')">🏠 Back to Menu</button>
+      <button class="back-menu" onclick="show('menu')">🏠 Back to Menu</button>
       </div>
     `;
     document.body.appendChild(el);
